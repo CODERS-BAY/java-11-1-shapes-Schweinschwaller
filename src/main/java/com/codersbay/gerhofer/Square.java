43dcd47ecb0052d5477bc21d4b985f2edfdbf2f7
Georg Schweinschwaller <georg.schweinschwaller@live.at>
2020-11-23 08:32:45 +0100

    Square is now a subclass of Rectangle

--- src/main/java/com/codersbay/gerhofer/Square.java
+++ src/main/java/com/codersbay/gerhofer/Square.java
@@ -16,18 +16,4 @@ public class Square extends Rectangle {
         super.setLength(length);
         super.setWidth(length);
     }
-
-    public double getArea() {
-        if (super.getLength() <= 0.0) {
-            throw new IllegalArgumentException("length is negativ.");
-        }
-        return super.getLength() * super.getLength();
-    }
-
-    public double getPerimeter() {
-        if (super.getLength() <= 0.0) {
-            throw new IllegalArgumentException("length is negativ.");
-        }
-        return super.getLength() * 4;
-    }
 }
